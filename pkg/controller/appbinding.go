7f18e249d3a1700231be84260fea2d1ca78424fc
Emruz Hossain <emruz@appscode.com>
2021-01-01 11:28:25 +0600

    Fix annotations passing to AppBinding (#24)
    Signed-off-by: Emruz Hossain <emruz@appscode.com>

--- pkg/controller/appbinding.go
+++ pkg/controller/appbinding.go
@@ -93,7 +93,7 @@ func (c *Controller) ensureAppBinding(db *api.MariaDB) (kutil.VerbType, error) {
 		func(in *appcat.AppBinding) *appcat.AppBinding {
 			core_util.EnsureOwnerReference(&in.ObjectMeta, owner)
 			in.Labels = db.OffshootLabels()
-			in.Annotations = meta_util.FilterKeys(kubedb.GroupName, in.Annotations, db.Annotations)
+			in.Annotations = meta_util.FilterKeys(kubedb.GroupName, nil, db.Annotations)
 
 			in.Spec.Type = appmeta.Type()
 			in.Spec.Version = dbVersion.Spec.Version
